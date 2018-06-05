d8dbcae08000dbb07dcad0db792c8b2b4e3f2ec1
EunbeeLee <lob1224@hanmail.net>
2018-06-05 18:17:19 +0900

    24. 캘린더 새일정 추가 테스트 2

--- src/main/java/kr/co/lac/schedule/controller/ScheduleController.java
+++ src/main/java/kr/co/lac/schedule/controller/ScheduleController.java
@@ -4,22 +4,28 @@ package kr.co.lac.schedule.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import kr.co.lac.repository.domain.Schedule;
 import kr.co.lac.schedule.service.ScheduleService;
 
 
-@RestController
+@Controller
 @RequestMapping("/schedule")
 public class ScheduleController {
 	@Autowired
-	private ScheduleService scheduleservice;  
+	private ScheduleService scheduleService;  
 	
-	@RequestMapping("/newSchedule.do")
-	public void newSchedule(Schedule sch) throws Exception {
-		scheduleservice.insertSchedule(sch);
-		
+	@RequestMapping("/schedule.do")
+	private void schedule(){}
+	
+	@RequestMapping("/newSchedule.json")
+	@ResponseBody
+	public Schedule newSchedule(Schedule sch) throws Exception {
+//		System.out.println("스케쥴 들어옴");
+		scheduleService.insertSchedule(sch);
+		return sch;
 	}
 	
 }
